4999b5ca7f788f184af6a7677a14aaae564c58b9
mbelay7835 <69566836+mbelay7835@users.noreply.github.com>
2021-02-03 01:20:50 -0600

    progress 2

---
+++ js/add.js
@@ -0,0 +1,46 @@
+//const fetch = require("node-fetch");
+
+async function addBook() {
+
+    let isbn = document.getElementById("isbn").value;
+    let title = document.getElementById("title").value;
+    let overdue = document.getElementById("Overdue").value;
+    let publisher = document.getElementById("publisher").value;
+    let publishDate = document.getElementById("date").value;
+
+    try {
+        const response = await fetch("https://elibraryrestapi.herokuapp.com/elibrary/api/book/add", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({
+
+                "isbn": isbn,
+                "title": title,
+                "overdueFee": overdue,
+                "publisher": publisher,
+                "datePublished": publishDate
+
+            })
+        })
+       // console.log(response)
+        if (response.status == 200) {
+            alert("You sucessfuly added book")
+        } else {
+            alert("Please add data in the required field")
+        }
+
+    } catch (err) {
+        alert(err)
+    }
+
+
+
+}
+//addBook()
+let saveBook = document.getElementById("save");
+
+saveBook.addEventListener("click", addBook);
+
+
--- js/books.js
+++ js/books.js
@@ -1,14 +1,27 @@
-const fetch = require("node-fetch");
+//const fetch = require("node-fetch");
+
 async function fetchDataJSON() {
     try {
-        const response = await fetch("https://elibraryrestapi.herokuapp.com/elibrary/api/book/list");//return header and body
-        //response.header.get()..to get the header
+        const response = await fetch("https://elibraryrestapi.herokuapp.com/elibrary/api/book/list"); //return header and body
+
         const data = await response.json();// return parsed body
-        console.log(data)
+console.log(data)
+        let table = document.getElementById('myTable')
+
+        for (let i = 0; i < data.length; i++) {
+            let row = `<tr>
+                    <td>${data[i].bookId}</td>
+                    <td>${data[i].isbn}</td>
+                    <td>${data[i].title}</td>
+                    <td>${data[i].overdueFee}</td>
+                    <td>${data[i].publisher}</td>
+                    <td>${data[i].datePublished}</td>
+                  </tr>`
+            table.innerHTML += row
+        }
 
     } catch (err) {
-        console.log(err)
+        document.getElementById("div").innerHTML = "Not successfully connected, check the url."
     }
 }
-
 fetchDataJSON()
